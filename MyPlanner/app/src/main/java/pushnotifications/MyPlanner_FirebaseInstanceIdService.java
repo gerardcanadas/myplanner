81d68c3e4dde42765544cd544de2381c2eb0281b
gerardcanadas <gerard.canadas@gmail.com>
2017-05-01 16:50:50 +0200

    set everything up for Firebase client

---
+++ MyPlanner/app/src/main/java/pushnotifications/MyPlanner_FirebaseInstanceIdService.java
@@ -0,0 +1,31 @@
+package pushnotifications;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by gerard on 5/1/17.
+ */
+
+public class MyPlanner_FirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    public void sendRegistrationToServer(String refreshedToken) {
+
+    }
+}
